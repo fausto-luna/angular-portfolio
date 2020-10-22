275274f29c82bdc78724b32b8554d2eab82d4788
Fausto Luna <faustoluna@me.com>
2020-10-22 17:17:14 +0200

    optimising photos

--- src/assets/ts/rockets/thruster-model.ts
+++
@@ -1,9 +0,0 @@
-class Thruster{
-    public currentPower:number;
-    public maxPower:number;
-
-    constructor(currentPower:number, maxPower:number){
-        this.currentPower = currentPower;
-        this.maxPower = maxPower;
-    }
-}
